1b221b4db50eeea22d1d303e0e4c2f52bd827db4
elnika1993 <shubladze.nikoloz@yahoo.com>
2022-02-05 02:47:24 +0400

    Initial commit

---
+++ amocanaN1.py
@@ -0,0 +1,5 @@
+def birthyear():
+    a = int(2022)
+    b = int(input("Enter year of birth: "))
+    print("your age is:", a-b)
+birthyear()
---
+++ amocanaN2.py
@@ -0,0 +1,32 @@
+x = (input("Enter name and sname: "))
+m = (input("Enter age: "))
+a = (input("Enter corona type:\n1 for Delta, 2 for Omicron, 3 for Deltacron: "))
+t = float(input("Enter temperature: "))
+z = {"1": "delta", "2": "omicron", "3": "deltacron"}
+
+
+def corona():
+
+    def stat():    #Covid passport (boloshi gamoitans sul)
+        print("")
+        print("Covid Passport:\nName,Sname:", x, "\nAge:", m, "\nTemperature:", t,
+              "\nCovid Status: POSITIVE", "\nCarantin time: 2 weeks!")
+
+    if a == "1" and t >= 36.5 and t <=37.5:
+        print("You have", z["1"], "\nTemperature is normal! Stay home.")
+        stat()
+    elif a == "1" and t >= 37.6:
+        print("You have", z["1"], "\nTemperature is NOT normal! Call 911.")
+        stat()
+    elif a == "2" and t >= 36.5 and t <= 37.5:
+        print("You have", z["2"], "\nTemperature is normal! Stay home.")
+        stat()
+    elif a == "2" and t >= 37.6:
+        print("You have", z["2"], "\nTemperature is NOT normal! Call 911.")
+        stat()
+    elif a == "3":
+        print("deltacron does not exist! ")
+    else:
+        print("Enter temperature more accurate! ")
+
+corona()
